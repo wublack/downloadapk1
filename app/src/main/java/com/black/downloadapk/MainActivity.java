b46c441fead3f9707fa55ada0e2fd039bc9e7742
wublack <wl52056@163.com>
2017-06-08 22:27:58 +0800

    df
    df

--- app/src/main/java/com/black/downloadapk/MainActivity.java
+++ app/src/main/java/com/black/downloadapk/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = this;
         //fff
+        //几个月了荒废了 测试
         initViews();
     }
 
